602e240a38e40c19ba180d0fb86c4345b4254f07
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-03-15 19:17:07 +0500

    some js projects

---
+++ Worldwide Holidays/main.js
@@ -0,0 +1,65 @@
+const apiKey = '292fc7b3bcaf765e5ce6b7928490eaf268c13dc1';
+const url = 'https://calendarific.com/api/v2/holidays?';
+
+function urlToFetchFun(country, year) {
+    return `${url}api_key=${apiKey}&country=${country}&year=${year}`;
+}
+
+const getHolidays = async (urlToFetch) => {
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const holidays = jsonResponse.response.holidays.map(holiday => {
+                obj = {
+                    name: holiday.name,
+                    date: holiday.date.iso,
+                }
+                function formatDate(date) {
+                    let year = date.getFullYear();
+                    let month = date.getMonth();
+                    let dt = date.getDate();
+                    const monthNames = [
+                        "Jan", "Feb", "Mar",
+                        "Apr", "May", "Jun",
+                        "Jul", "Avg", "Sep",
+                        "Oct", "Nov", "Dec"
+                    ];
+                    const newDate = dt + " " + monthNames[month] + " " + year;
+                    return newDate;
+                }
+
+                const newDate = formatDate(new Date(obj.date));
+                obj.date = newDate;
+                return obj;
+            });
+
+            console.log(holidays);
+            return holidays;
+        }
+        else {
+            throw new Error('Request Failed!');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const createHolidaysHTML = (holiday) => {
+    return `<p><span class="left">"<b>${holiday.name}</b>"</span> <span class="right">${holiday.date}</span></p>`;
+};
+
+function renderHolidays(holidays) {
+    let holidaysContent = "";
+    for (let i = 0; i < holidays.length; i++) {
+        holidaysContent += createHolidaysHTML(holidays[i]);
+    }
+    
+    return holidaysContent;
+}
+
+function executeSearch(executeCountry, executeYear) {
+    const result = getHolidays(urlToFetchFun(executeCountry, executeYear)).then(holidays => renderHolidays(holidays));
+    console.log(result);
+    return result;
+}
